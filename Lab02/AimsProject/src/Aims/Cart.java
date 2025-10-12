f772c313834c65368a83191d8b33b685156e4865
ntm8326 <ntm8326@gmail.com>
2025-10-12 23:40:28 +0700

    Add file to project

---
+++ Lab02/AimsProject/src/Aims/Cart.java
@@ -0,0 +1,59 @@
+package Aims;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0;
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+			System.out.println("The cart is full");
+		}
+		else if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+			itemsOrdered[qtyOrdered] = disc;
+			qtyOrdered++;
+			System.out.println("The disc " + disc.getTitle() + " has successfully added!");
+			
+		}
+	}
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean isEmpty = true;
+        boolean found = false;
+        for(int j = 0; j < qtyOrdered; j++) {
+            if(itemsOrdered[j] != null) {
+                isEmpty = false;
+                break;
+            }
+        }
+        if (qtyOrdered == 0 || isEmpty) {
+            System.out.println("This cart has no item yet!");
+        }
+        else {
+        	for(int i = 0; i < qtyOrdered; i++) {
+                if(itemsOrdered[i] == disc) {
+                    itemsOrdered[i] = null;
+                    System.out.println("The disc " + disc.getTitle() + " has successfully removed!");
+                    found = true;
+                    for(int j = i; j < qtyOrdered - 1; j++) {
+                    	DigitalVideoDisc temp = itemsOrdered[i];
+                    	itemsOrdered[i] = itemsOrdered[i+1];
+                    	itemsOrdered[i+1] = temp;
+                    }
+                    qtyOrdered--;
+                    break;
+                    }
+                }
+             if (!found) {
+                 System.out.println("Disc not found in the cart");
+             }
+        }
+    }
+	public float totalCost() {
+		float total = 0;
+		for(int i = 0; i < qtyOrdered; i++) {
+			if (itemsOrdered[i] != null) {
+				total += itemsOrdered[i].getCost();
+			}
+		}
+		return total;
+	}
+}
